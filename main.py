7d3a50e9bd5ef5527e12a6e0a78ef0581ba3df00
«HaykOsyan <ohayk@mail.ru>
2024-08-15 11:15:53 +0400

    task2 Poly

--- main.py
+++ main.py
@@ -26,15 +26,35 @@ array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
 #         continue    
 #     else : print("elements in array_1 which are not in array_2 and array_3", i)
 
-array_1 = {1, 5, 10, 20, 40, 80, 100}
-array_2 = {6, 7, 20, 80, 100}
-array_3 = {3, 4, 15, 20, 30, 70, 80, 120}
+# array_1 = {1, 5, 10, 20, 40, 80, 100}
+# array_2 = {6, 7, 20, 80, 100}
+# array_3 = {3, 4, 15, 20, 30, 70, 80, 120}
 
-intersection_array = array_1 & array_2 & array_3
+# intersection_array = array_1 & array_2 & array_3
 
-print(list(intersection_array))
+# print(list(intersection_array))
 
-difference_array = array_1 - array_2 -array_3
+# difference_array = array_1 - array_2 -array_3
 
-print(list(difference_array))
-    
+# print(list(difference_array))
+
+"""
+Задача 2. Палиндром
+Пользователь вводит строку. Необходимо написать программу, которая
+определяет, существует ли у этой строки перестановка, при которой она станет
+палиндромом. Затем она должна выводить соответствующее сообщение.
+"""
+
+print('enter a word or letters')
+str = input()
+def is_poly(string) :
+    unique = 0
+    for i in str :
+        if(str.count(i) % 2 != 0) :
+            unique += 1
+    if(unique <= 1) :
+        print("poly")
+    else :
+        print("not poly")
+        
+is_poly(str)
